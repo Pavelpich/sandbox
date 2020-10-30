0c618962799e40569871c85a5087463d0ff6b413
pavel <Pavelpichugin28@gmail.com>
2020-10-30 14:41:59 +0300

    первый комит сделали класс кузнеца

---
+++ Javafirstprogram/gradleproject1/src/main/java/gradleproject1/Blacksmith.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradleproject1;
+
+import FuckUtiles.Condom;
+import FuckUtiles.Orgasm;
+import FuckUtiles.AwesomeOrgasm;
+import Entities.Chair;
+import Entities.Money;
+import interfaces.Worker;
+
+
+/**
+ *
+ * @author pavel
+ */
+public class Blacksmith implements Worker{
+    
+    public String makerain () {
+        return "dozhdik";
+}
+    private Orgasm fuckme(Condom condom ){
+        return new Orgasm();
+    }
+    
+    private AwesomeOrgasm fuckme(){
+        return new AwesomeOrgasm();
+    }
+    
+    public Blacksmith (){
+        int a = 123;
+        System.out.println(a);
+              
+    }
+    
+    public Chair makeChair(Money money){
+        return makeChair();
+    }
+    
+    private Chair makeChair(){
+        return new Chair();
+    }
+    
+    
+    public boolean doubleExpression (double a, double b, double c) {
+   
+        if (a + b == c)
+        {
+            return true;
+        }
+    return false;
+        
+
+}
+
+    
+}
---
+++ Javafirstprogram/gradleproject1/src/main/java/gradleproject1/Main.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradleproject1;
+import Entities.Chair;
+import Entities.Money;
+import java.security.MessageDigest;
+
+
+/**
+ *
+ * @author pavel
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        
+        double a = new Double(5656465);
+        double b = 345.42594;
+        double c = a+b;
+        Blacksmith boba = new Blacksmith();
+        boolean d = boba.doubleExpression(a, b, c);
+        
+        
+        int f = Integer.MAX_VALUE+1;
+        System.out.println(d);
+        
+        Money money = new Money();
+        boba.makeChair(money);
+        Chair goodchair = boba.makeChair(money);
+                                
+//        MessageDigest md = MessageDigest.getInstance("MD5");
+//        byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
+//        for (byte b : digest) {
+//            System.out.printf("%02x", b);
+    }
+ }
+
